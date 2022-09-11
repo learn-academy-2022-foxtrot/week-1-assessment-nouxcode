16b958646da691a7dea776e72d2dcf6a8c52e64c
nouxcode <perrault.meaghan@gmail.com>
2022-09-11 15:43:27 -0700

    Week 1 Assessment Completed

--- code-challenges.js
+++ code-challenges.js
@@ -21,6 +21,11 @@ const fruit2 = "banana"
 // --------------------1) Create the code that determines if a given number is below boiling point, at boiling point, or above boiling point. Boiling point is 212 degrees Fahrenheit. Use the test variables provided below.
 
 // Describe your process:
+// In order to create the code that determines if a given number is below boiling point, at boiling point, or above boiling point I will first need to establish the variable with the value of what is considered the boiling point. 
+//In this case, the boiling point variable will be valued at 212 degrees fahrenheit. 
+//I will write an "if" statement for below boiling point, an "else if" statement for at boiling point, and a seperate "else if" statement if the test variable is above boiling point. 
+//Each "if", and "else if" statement will be followed by a "console.log" and in parenthesis I will designate each boiling level.
+
 
 const temp = 42
 // Expected output: "42 is below boiling point"
@@ -31,24 +36,63 @@ const temp = 42
 // const temp = 212
 // Expected output: "212 is at boiling point"
 
+if (temp < 212) {
+    console.log(`${temp}° is below the boiling point!`);
+  } else if (temp === 212) {
+    console.log(`${temp}° is at the boiling point!`);
+  } else if (temp > 212) {
+    console.log(`${temp}° is above the boiling point!`);
+
+// END RESULT:
+//All good after running node in the terminal. Expect output generated.
+
 // --------------------2) Create the code that will combine the two arrays and return the length using the test variables provided below.
 
 // Describe your process:
+// In order to create the code that will combine the two arrays and return the length using the test variables provided below I will need to utilize the .concat() method to merge the two arrays. 
+//By doing this the original array(s) is unchanged and just combined. 
+//I will use the "let" command to establish each array to designate the Padres 1984, and Padres 1988 World Series Runs, and then use the "console.log" command in conjuction with the .concat command followed by the .length. 
+//Afterwards I will use "comboArrays" to establish the variable for the combined arrays.
 
 const padres1984WorldSeriesRuns = [2, 5, 2, 2, 4]
 const padres1998WorldSeriesRuns = [6, 3, 5, 3]
 // Expected output: 9
 
+let padresTotalRuns = padres1984WorldSeriesRuns.concat(
+    padres1998WorldSeriesRuns
+  ).length;
+  console.log(padresTotalRuns)
+
+// END RESULT:
+//All good after running node in the terminal. Expect output generated.
+
+
+
 // --------------------3) Create the code that will reverse the letters of a string using the test variable provided below.
 
 // Describe your process:
+// In order to create the code that will reverse the letters of a string using the test variable providied below, I will need to utilize the .reverse() method following my the currentCohort variable.
 
 const currentCohort = "Foxtrot 2022"
 // Expected output: "2202 tortxoF"
 
+//console.log(currentCohort.reverse())
+
+
+// END RESULT:
+// When I ran node in the terminal it rendered that .reverse() is not a function, I'm not sure as to why after troubleshooting.
+// I posted the issue I was experiecing in Slack and a few classmates and an instructor were about to help me troubleshoot further, here is the code that eventually became successful for me:
+
+console.log(currentCohort.split("").reverse().join(""))
+
+// END RESULT:
+//All good after running node in the terminal. Expect output generated.
+
 // --------------------4) Create the code that will return the last index of the given value from the array using the test variables provided below.
 
 // Describe your process:
+// In order to create the code that will return the last index of the given value from the array using the test variables provided below I use the .lastIndexOf method following the "console.log" command.
+//Followinf the "console.log" command I will put in parenthesis "myNumbers.lastIndexOf" and then within that I will put in another set of parenthesis "givenValue1"
 
 const myNumbers = [13, 34, 42, 5, 5, 10, 27, 42, 10]
 const givenValue1 = 42
@@ -57,12 +101,24 @@ const givenValue1 = 42
 const givenValue2 = 10
 // Expected output: 8
 
+console.log(myNumbers.lastIndexOf(givenValue1))
+
+// END RESULT:
+//All good after running node in the terminal. Expect output generated.
+
 // --------------------5) Create the code that will sort all the numbers in an array from largest to smallest. Use the test variables provided below.
 
 // Describe your process:
+// In order to create the code that will sort all the numbers in an array from largest to smallest using the test variables I will utilize the ".sort()" method.
+//I will need to provide the method with an argument since the ".sort()" doesn't work correctly without the aide of an argument.
+//In this case I will also utilize the ".reverse()" method
 
 const sanDiegoSummerTemperatures = [79, 80, 72, 73, 82, 77, 76]
-// Expected output: [82, 80, 79, 77, 76, 73, 72]
+console.log(sanDiegoSummerTemperatures.sort().reverse())
+
 
 const sanDiegoWinterTemperatures = [59, 68, 62, 59, 66, 67, 66]
-// Expected output: [68, 67, 66, 66, 62, 59, 59]
+console.log(sanDiegoWinterTemperatures.sort().reverse())
+  }
+// END RESULT:
+//All good after running node in the terminal. Expect output generated.
--- interview-questions.md
+++ interview-questions.md
@@ -8,50 +8,50 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 INSTRUCTOR EXAMPLE: What is a conditional statement?
 
-Your answer:
+Your answer: 
 
 Researched answer:
 
 1. What is git? What is the difference between git and Github?
 
-Your answer:
+Your answer: Git is software thats open-source which allows you to keep track of your source code and GitHub is the cloud service Git repository. GitHub allows for code hosting and collaboration for developers.
 
-Researched answer:
+Researched answer: Git is a version control system which is open-source and helps you manage the history of your source code, while Github is the cloud hosting service which manages your repositories from Git. 
 
 2. Which JavaScript operators will return a Boolean value?
 
-Your answer:
+Your answer: The JavaScript operators that will return a Boolean value are logical operators.
 
-Researched answer:
+Researched answer: ThecJavaScript operators that will return a Boolean values are AND, OR, and NOT. These values are considered logical operators.
 
 3. What is an index? What is the difference between index and value?
 
-Your answer:
+Your answer: The index is the amount of time to skip, counting from the start of an array. Values are basically portions seperate bits of information that play different roles.
 
-Researched answer:
+Researched answer: The index is the placement of strings and their property length that are accounted for. Strings are considered "zero indexed" therefore the counting of the characters begins with 0 and is counted from there. A value is a chunk of information when you set a variable, it can be any data type and can be changed at any time.
 
 4. Compare and contrast arrays and strings.
 
-Your answer:
+Your answer: Arrays are a type of variable that can have multiple values at a time and strings are a series of characters which represent words as opposed to numbers.
 
-Researched answer:
+Researched answer: The primary difference between an array and a string is that an array is a data structure that contains a collection of elements having the same data types. A String is a collection of textm, words or characters.
 
 5. Give a brief description of proper pair programming techniques. What are the roles of each person?
 
-Your answer:
+Your answer: Proper pair programming techniques utilize two primary roles, which are the driver and navigator and incorporate timed breaks to minimize fatigue roughly every 10-25 minutes or so depending on preference and frequently switching roles. The driver is the person designated to write the code while the navigator is an additional set of eyes for catching any errors with the code by reviewing and proofreading. 
 
-Researched answer:
+Researched answer: A proper pair programming technique that is utilized is known as Pomdoro Pairing. In this method a 25 minute timer is started, one person takes on the role of the driver (person who writes the code) while the other person is designated as the navigator (person reviewing the codes for any errors) and work is conducted until the time runs out. Afterwards the pair takes a 5-10 minute break and then they switch roles so that the person that was previously driving then navigates and vice versa.
 
 ## Looking Ahead: Terms for Next Week
 
 Research and define the following terms to the best of your ability.
 
-1. Higher Order Functions:
+1. Higher Order Functions: Higher Order functions are functions that operate on other functions by taking them as arguments or by returning them. Higher order functions allow for abstraction over actions and not just values, and they come in several forms. FOr instance, we can have functions which create new functions.
 
-2. Jest:
+2. Jest: Jest is a testing framework in JavaScript that allows the user to check the correctness of any codebase in JavaScript. It allows the user to to code and write tests that will generate as pass of fail.
 
-3. Objects:
+3. Objects: Objects are arbitrary collections of properties, and they are created by using braces as an expression.
 
-4. Method:
+4. Method: A method is a property that contains function values. Typically a method needs to do something with the object which it was called on.
 
-5. Classes:
+5. Classes: Classes define the shape of a type of object and what methods and properties it has.
